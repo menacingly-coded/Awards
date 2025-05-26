e2af48ad6ec5b203df6c1d494b33f472efeb494b
Shreya Sharma <shreya0987.edu@gmail.com>
2025-05-26 14:44:58 +0530

    Initial commits

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+const App = () => {
+    return <main>
+        <h1 className="text-5xl text-orange-500 font-bold text-blue-50">Welcome to Awards</h1>
+    </main>
+}
+export default App;

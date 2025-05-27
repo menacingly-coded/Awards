3f367fd74c58673e412b96229dbbae9334e9e51a
Shreya Sharma <shreya0987.edu@gmail.com>
2025-05-27 16:13:29 +0530

    modified Hero and added About

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
       fontFamily:{
         zentry: ['zentry','sans-serif'],
         general: ['general', 'sans-serif'],
-        'circular-web': ['circular-web', 'sans-seri'],
+        'circular-web': ['circular-web', 'sans-serif'],
         'robert-medium': ['robert-medium', 'sans-serif'],
         'robert-regular': ['robert-regular', 'sans-serif']
       },
